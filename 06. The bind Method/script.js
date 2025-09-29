cbe14b6967a03f4e4cf57dfe858c954f4c7b3d45
ephraim <ephraims.818@gmail.com>
2025-09-29 06:09:36 +0600

    JS-REVICE--COMPLETE-JS---FUNCTIONS - 06. The bind Method

---
+++ 06. The bind Method/script.js
@@ -0,0 +1,88 @@
+'use strict';
+
+const lufthansa = {
+  airline: 'Lufthansa',
+  iataCode: 'LH',
+  bookings: [],
+
+  book(flightNum, name) {
+    console.log(
+      `${name} booked a seat on ${this.airline} flight ${this.iataCode}${flightNum}`
+    );
+    this.bookings.push({ flightNum: `${this.iataCode}${flightNum}`, name });
+  },
+};
+
+lufthansa.book(239, 'Ephraim S');
+lufthansa.book(635, 'John Smith');
+console.log(lufthansa);
+
+const eurowings = {
+  airline: 'Eurowings',
+  iataCode: 'EW',
+  bookings: [],
+};
+
+const book = lufthansa.book;
+
+// Call method
+book.call(eurowings, 23, 'Sarah Williams');
+console.log(eurowings);
+
+book.call(lufthansa, 239, 'Mary Cooper');
+console.log(lufthansa);
+
+const swiss = {
+  airline: 'Swiss Airlines',
+  iataCode: 'LX',
+  bookings: [],
+};
+
+book.call(swiss, 583, 'Mary Cooper');
+console.log(swiss);
+
+// Apply method
+const flightData = [583, 'George Cooper'];
+book.apply(swiss, flightData);
+console.log(swiss);
+
+// Bind method
+const bookEW = book.bind(eurowings);
+const bookLH = book.bind(lufthansa);
+const bookLX = book.bind(swiss);
+
+bookEW(23, 'Steven Williams');
+
+const bookEW23 = book.bind(eurowings, 23);
+bookEW23('Ephraim S');
+bookEW23('Martha Cooper');
+
+// With Event listener
+lufthansa.planes = 30;
+lufthansa.buyPlane = function () {
+  this.planes++;
+  console.log(this.planes);
+};
+
+document
+  .querySelector('.buy')
+  .addEventListener('click', lufthansa.buyPlane.bind(lufthansa));
+
+const addTax = (rate, value) => {
+  return value + value * rate;
+};
+
+console.log(addTax(0.1, 200));
+
+const addVat = addTax.bind(null, 0.23);
+console.log(addVat(100));
+console.log(addVat(23));
+
+// Challenge
+const addVat_2 = function (rate) {
+  return function (value) {
+    return value + value * rate;
+  };
+};
+
+console.log(addVat_2(0.2)(500));
